c4b5cf09ceed1a127998dc9d4aa2eaa08f491b96
George Gangos <george.gangos@yahoo.com>
2025-10-25 10:19:50 -0400

    Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    if (!username || !password) {
+      return res.send("Username and password are required");
+    }
+    
+    if (isValid(username)) {
+      return res.send("User already exists");
+    }
+    
+    users.push({ username: username, password: password });
+    return res.send("User successfully registered");
 });
 
 // Get the book list available in the shop

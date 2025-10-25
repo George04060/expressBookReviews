65200f7002333df06c71c1a955adb7611737c711
George Gangos <george.gangos@yahoo.com>
2025-10-25 09:41:01 -0400

    Task 1

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,29 @@ const regd_users = express.Router();
 
 let users = [];
 
+function generateAccessToken(username) {
+    return jwt.sign({data: username }, "access", {expiresIn: "1h" });
+}
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    return users.filter(user => user.username === username).length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    return users.filter(user => user.username === username && user.password === password).length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } =  req.body;
+
+    if (authenticatedUser(username, password)) {
+        let accessToken =  generateAccessToken(username);
+        req.session.authorization = { accessToken };
+        return res.status(200),express.json({message: "User successfully logged in" });
+    } else {
+        return res.status(403),express.json({message: "Invalid credentials" });
+    }
+
 });
 
 // Add a book review
